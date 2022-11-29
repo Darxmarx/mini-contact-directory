fb799011e7b8c149159df01b0a78ce813e855b68
Darx <mcrivabode@gmail.com>
2022-11-29 12:05:02 -0800

    further progress on service-worker generation, start on database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,24 +2,31 @@
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
-
-};
-
+const initdb = async () =>
+    openDB('cards', 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains('cards')) {
+                console.log('cards db already exists');
+                return;
+            }
+            db.createObjectStore('cards', { keyPath: 'id', autoIncrement: true });
+            console.log('cards db created!');
+        }
+    });
 
 // TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
+export const postDb = async (name, home, cell, email) => {
+
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+
 };
 
 initdb();
